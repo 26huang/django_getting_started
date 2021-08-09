90e697f668aff3ca3b6a169af3eab08d2b6befbf
26huang <weishuoh@gmail.com>
2021-08-09 00:24:52 -0400

    added form example

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Meeting, Room
+from django.forms import modelform_factory
 
 # Create your views here.
 
@@ -10,3 +11,19 @@ def detail(request, id):
 
 def rooms(request):
     return render(request, "meetings/rooms_info.html", {"rooms": Room.objects.all()})
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
+
+
+    form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
